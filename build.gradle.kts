e95814b00399af0e8b0b3b03442a5c486d92f8c7
Gauweiler42 <leon@lgauweiler.de>
2023-10-27 23:02:23 +0200

    added: java files in src folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -277,7 +277,7 @@ dependencies {
     // HttpGraphQlClient benoetigt WebClient mit Project Reactor
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-json")
-    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    //implementation("org.springframework.boot:spring-boot-starter-graphql")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
